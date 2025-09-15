6ad5bba17333584c0c5dd1a0b7b1e876dda60134
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-09-15 18:41:58 +0530

    Cardthree updated

--- src/components/Cardthree.jsx
+++ src/components/Cardthree.jsx
@@ -1,8 +1,8 @@
 const Cardthree=()=>{
     return(
         <div style={{backgroundColor:"#FCA201"}} className='px-10 py-5 border rounded-md my-3 text-center flex-grow'>
-            <h1>Built Using</h1>
-            <p>React JS</p>
+            <h1 className="text-2xl font-medium mb-1">Built Using</h1>
+            <p className="text-xl">React JS</p>
         </div>
     );
 }
