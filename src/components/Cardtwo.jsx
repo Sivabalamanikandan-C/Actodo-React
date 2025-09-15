b9f548c22dcac68ac14da33a3632127cee75ddc4
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-09-15 18:40:22 +0530

    Cardtwo updated

--- src/components/Cardtwo.jsx
+++ src/components/Cardtwo.jsx
@@ -17,7 +17,7 @@ const Cardtwo = () => {
     return (
         <div className='bg-red-400 px-10 py-5 border rounded-md my-3 text-center flex-grow'>
             <h1 className="text-2xl font-medium mb-1">{dateTime.toLocaleDateString("en-US", { month: "long", day: "numeric" })}</h1>
-            <p>{dateTime.toLocaleTimeString()}</p>
+            <p className="text-xl">{dateTime.toLocaleTimeString()}</p>
         </div>
     );
 };
