209e417bd3472cf6a1ebf637898576497e2f083d
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-09-20 13:10:18 +0530

    Cardthree updated

--- src/components/Cardthree.jsx
+++ src/components/Cardthree.jsx
@@ -1,6 +1,6 @@
-const Cardthree=()=>{
-    return(
-        <div style={{backgroundColor:"#FCA201"}} className='px-10 py-5 border rounded-md my-3 text-center flex-grow'>
+const Cardthree = () => {
+    return (
+        <div style={{ backgroundColor: "#FCA201" }} className='px-10 py-5 border rounded-md my-3 text-center flex-grow'>
             <h1 className="text-2xl font-medium mb-1">Built Using</h1>
             <p className="text-xl">React JS</p>
         </div>
