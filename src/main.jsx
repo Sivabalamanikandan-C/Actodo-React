fea9678dd18ef9dcd143f6abcd3a9803957a46bb
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-10-06 19:17:05 +0530

    Project updation

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="bg-yellow-200 text-black text-center p-2">
-
+      🚧 This project is under development. Features may change or not work properly.
     </div>
   </StrictMode>,
 )
