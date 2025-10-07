c3092b2677e3103c5951bb5a27244c7c9aff2970
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-10-07 18:53:29 +0530

    Updated UI components

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <div className="bg-yellow-200 text-black text-center p-2">
+    <div className="bg-yellow-200 text-black text-center p-2">
       🚧 This project is under development. Features may change or not work properly.
-    </div> */}
+    </div>
 
-    <App/>
+    {/* <App/> */}
   </StrictMode>,
 )
