1947d3fd5669e7408a1dfa6d5d02fe36102619b6
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-10-08 10:03:43 +0530

    Updated UI components

--- src/components/Addtodoform.jsx
+++ src/components/Addtodoform.jsx
@@ -20,7 +20,7 @@ const Addtodoform = (props) => {
         <div className="text-center">
             <h1 className="text-2xl font-medium mt-2 mb-1">Manage Activity</h1>
             <div className="mb-3">
-                <input type="text" value={newActivity} onChange={handlenewactivity} placeholder="Next Activity?" className="outline-1 p-1 my-2 rounded-md bg-transparent" />
+                <input type="text" value={newActivity} onChange={handlenewactivity} placeholder="Next Activity?" className="outline-2 p-1.5 my-2 rounded-md bg-transparent" style={{width:'40%'}} />
                 <button onClick={handleChange} className="bg-black text-white px-2 py-1 ml-2 rounded-md">Add</button>
             </div>
         </div>
