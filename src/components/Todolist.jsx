ee9b3db98e04201a4a463731f7519afef3563149
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2025-10-16 19:48:43 +0530

    UI components updated

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -8,7 +8,7 @@ const Todolist = (props) => {
 
     return (
         <div className="bg-[#BDB4EA] border rounded-md p-2 flex-grow md:ml-6">
-            <h1 className="text-2xl font-medium mb-2">Today's Activity</h1>
+            <h1 className="text-2xl font-medium mb-3 text-center md:text-left">Today's Activity</h1>
             {
                 activityArr.length === 0 ? <p>You haven't added anything yet...</p> :
                     <div>
